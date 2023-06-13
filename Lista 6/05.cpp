e5ac45291fdb16653e01227c9016d4ab11f307ff
Fernanda Felix <118316479+im-fernanda@users.noreply.github.com>
2023-06-13 08:30:18 -0300

    Update 05.cpp

--- Lista 6/05.cpp
+++ Lista 6/05.cpp
@@ -1,5 +1,4 @@
-// Altere o exercício anterior para que, após o término de todos os cadastros, ou seja, quando 
-// o usuário digitar "N" na pergunta para novo cliente ou quando preencher o vetor com 20 
+// Altere o exercício anterior para que, após o término de todos os cadastros, ou seja, quando o usuário digitar "N" na pergunta para novo cliente ou quando preencher o vetor com 20 
 // clientes, a tela seja ser limpa e seja exibida uma nova tela perguntando se o usuário quer ver 
 // um relatório ou consultar um cliente individualmente. Se ele desejar ver o relatório, o 
 // sistema deverá exibi-lo conforme questão anterior. Se ele escolher consultar um cliente 
